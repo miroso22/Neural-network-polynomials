7d30780ec967e4486a780a1da016d440f3a5fe5e
Miroso02 <vaniastronov@gmail.com>
2020-05-27 16:12:01 +0300

    Added dependencies, eslint and packsge.json

--- Main.js
+++ Main.js
@@ -1,12 +1,15 @@
 'use strict';
 const NeuralNetwork = require('./Classes.js').NeuralNetwork;
+const process = require('process');
 const NUMBER_OF_EXAMPLES = 5;
+
+const nn = new NeuralNetwork(3, 1, 5, 1);
 
 const learningProcess = data => {
   for (const example of data) {
     nn.train([example.x], [example.y]);
   }
-}
+};
 
 const readline = require('readline');
 const data = [];
@@ -21,13 +24,11 @@ rl.on('line', input => {
   if (!isNaN(x) && !isNaN(y)) {
     if (data.length >= NUMBER_OF_EXAMPLES) {
       if (data.length === NUMBER_OF_EXAMPLES) learningProcess(data);
-      console.log('Expected: ' + Math.floor(nn.train([x], [y]) * 10))
+      console.log('Expected: ' + Math.floor(nn.train([x], [y]) * 10));
     }
     data.push({ x, y });
-  }
-  else console.log('Invalid input');
+  } else console.log('Invalid input');
 });
 
-const nn = new NeuralNetwork(3, 1, 5, 1);
 console.log(nn.calcValues(2)[0]);
 console.log(`Write ${NUMBER_OF_EXAMPLES} examples`);
