728fd0f8772811d30b024aa9f2ec5009ebdb1272
Miroso02 <vaniastronov@gmail.com>
2020-06-12 14:43:56 +0300

    Replaced all switch-cases with function collections

--- src/Main.js
+++ src/Main.js
@@ -8,29 +8,46 @@ const getPolynom = require('./PolynomInput.js').getPolynom;
 const readline = require('readline');
 const colors = require('colors');
 
-const inputCommands = {};
-
 const nn = new NeuralNetwork(1, 1);
-let consoleMode = 'c';
 const NORM_KOEF = 100;
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-//--------------------------- Input functions ----------------------------------
+//--------------------------- Values input ----------------------------------
 
-const invalInput = () => console.log('Invalid input. Enter h for help'.red);
-
-const generateExamples = fn => {
-  const data = [];
-  for (let i = 0; i < 100; i++) {
-    const x = Math.random() * 2 * NORM_KOEF - NORM_KOEF;
-    data[i] = [[x / NORM_KOEF], [fn(x) / NORM_KOEF]];
+const inputValues = {};
+let consoleMode = 'c';
+inputValues['c'] = input => {
+  const x = parseInt(input) / NORM_KOEF;
+  if (!isNaN(x)) {
+    const answer = Math.round(nn.calcValues([x])[0] * NORM_KOEF);
+    console.log('Result: '.cyan + `${answer}`.yellow);
   }
-  return data;
-}
+  else invalInput();
+};
+inputValues['t'] = input => {
+  const x = parseInt(input.split(' ')[0]) / NORM_KOEF;
+  const y = parseInt(input.split(' ')[1]) / NORM_KOEF;
+  if (!isNaN(x) && !isNaN(y)) {
+    const answer = Math.round(nn.train([x], [y])[0] * NORM_KOEF);
+    console.log('Expected answer: '.cyan + `${answer}`.yellow);
+    console.log();
+  } else invalInput();
+};
+inputValues['f'] = input => {
+  const fn = getPolynom(input);
+  if (fn) {
+    const dataSet = generateExamples(fn);
+    nn.trainOnDataset(dataSet);
+    consoleMode = 'c';
+  } else console.log('Invalid function input'.red);
+};
 
+//--------------------------- Command input ------------------------------------
+
+const inputCommands = {};
 inputCommands['h'] = () => { // help
   console.log('Commands:'.green);
   console.log('q - quit'.yellow);
@@ -56,42 +73,24 @@ inputCommands['f'] = () => { // enter a function
   console.log('For example, 5x^2-3 will be "5 0 -3"'.green);
 };
 
+//------------------------- Other methods --------------------------------------
+
+const invalInput = () => console.log('Invalid input. Enter h for help'.red);
+
+const generateExamples = fn => {
+  const data = [];
+  for (let i = 0; i < 100; i++) {
+    const x = Math.random() * 2 * NORM_KOEF - NORM_KOEF;
+    data[i] = [[x / NORM_KOEF], [fn(x) / NORM_KOEF]];
+  }
+  return data;
+}
+
 //--------------------------- Main code ----------------------------------------
 
 rl.on('line', input => {
-  if (inputCommands[input]) {
-    inputCommands[input]();
-  } else {
-    let x;
-    let y;
-    let fn;
-    switch (consoleMode) {
-      case 'c':
-        x = parseInt(input) / NORM_KOEF;
-        if (!isNaN(x)) {
-          const answer = Math.round(nn.calcValues([x])[0] * NORM_KOEF);
-          console.log('Result: '.cyan + `${answer}`.yellow);
-        }
-        else invalInput();
-        break;
-      case 't':
-        x = parseInt(input.split(' ')[0]) / NORM_KOEF;
-        y = parseInt(input.split(' ')[1]) / NORM_KOEF;
-        if (!isNaN(x) && !isNaN(y)) {
-          const answer = Math.round(nn.train([x], [y])[0] * NORM_KOEF);
-          console.log('Expected answer: '.cyan + `${answer}`.yellow);
-          console.log();
-        } else invalInput();
-        break;
-      case 'f':
-        fn = getPolynom(input);
-        if (fn) {
-          nn.trainOnDataset(generateExamples(fn));
-          consoleMode = 'c';
-        } else console.log('Invalid function input'.red);
-        break;
-    }
-  }
+  if (inputCommands[input]) inputCommands[input]();
+  else inputValues[consoleMode](input);
 });
 console.log('This application was created to show you an example of Neural Network learning'.yellow);
 console.log('Come up with some polynom, enter the exapmles of input-output pairs \n'.yellow +
